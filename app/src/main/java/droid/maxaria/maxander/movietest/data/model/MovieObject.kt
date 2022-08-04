b1bd02945c0ca7a4c38533b81382510f9d77b023
maxander3 <maxander234432@gmail.com>
2022-08-04 21:04:20 +0300

    add annotations

--- app/src/main/java/droid/maxaria/maxander/movietest/data/model/MovieObject.kt
+++ app/src/main/java/droid/maxaria/maxander/movietest/data/model/MovieObject.kt
@@ -1,15 +1,23 @@
 package droid.maxaria.maxander.movietest.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class MovieObject(
+    @SerializedName("items")
     val items: List<Item>,
 ) {
     data class Item(
+        @SerializedName("actors")
         val actors: List<Actor>,
+        @SerializedName("directorName")
         val directorName: String,
+        @SerializedName("releaseYear")
         val releaseYear: Int,
+        @SerializedName("title")
         val title: String,
     ) {
         data class Actor(
+            @SerializedName("actorName")
             val actorName: String,
         )
     }
