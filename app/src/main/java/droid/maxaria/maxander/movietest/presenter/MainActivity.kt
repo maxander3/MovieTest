9cc7afb4e79497eee004bed245852bb99c40d480
maxander3 <maxander234432@gmail.com>
2022-08-06 23:47:59 +0300

    add onBackPressed

--- app/src/main/java/droid/maxaria/maxander/movietest/presenter/MainActivity.kt
+++ app/src/main/java/droid/maxaria/maxander/movietest/presenter/MainActivity.kt
@@ -1,14 +1,27 @@
 package droid.maxaria.maxander.movietest.presenter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import droid.maxaria.maxander.movietest.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var onBackPressed: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        if (onBackPressed + 2000 > System.currentTimeMillis()) {
+            finish()
+        } else {
+            Toast.makeText(baseContext, R.string.click_again, Toast.LENGTH_LONG).show()
+        }
+        onBackPressed = System.currentTimeMillis()
     }
 }
